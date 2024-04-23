62a4595e439b4e6502e0444c163cddb76b3ee702
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-04-23 14:56:14 +0200

    added example, dealing with input molecules

--- src/smi3d.py
+++ src/smi3d.py
@@ -1,19 +1,51 @@
 import os
+import csv
 import argparse
 import subprocess
+import tempfile
+import shutil
+from rdkit import Chem
 
 root = os.path.dirname(os.path.abspath(__file__))
+
+tmp_dir = tempfile.mkdtemp(prefix='smi3d_')
 
 
 def main() -> None:
     args = parseArgs()
 
+    input_file = os.path.join(tmp_dir, 'input.sdf')
+    output_file = os.path.join(tmp_dir, 'output.sdf')
+
+    with open(args.in_file, 'r') as f:
+        reader = csv.reader(f)
+        smiles_list = [row[0] for row in reader]
+        if smiles_list[0].lower().startswith("smiles"):
+            smiles_list = smiles_list[1:]
+
+    idxs = []
+    mols = []
+    for i, smi in enumerate(smiles_list):
+        mol = Chem.MolFromSmiles(smi)
+        if mol is None:
+            continue
+        idxs += [i]
+        mol.setProp("_Index", str(i))
+        mols += [mol]
+
+    with Chem.SDWriter(input_file) as w:
+        for m in mols:
+            w.write(m)
+
     if args.num_confs == 1:
-        cmd = "python {0}/tools/gen_3d_structs.py -i {1} -o {2} -t {3}".format(root, args.in_file, args.out_file, args.max_time)
+        cmd = "python {0}/tools/gen_3d_structs.py -i {1} -o {2} -t {3}".format(root, input_file, output_file, args.max_time)
     else:
-        cmd = "python {0}/tools/gen_confs.py -i {1} -o {2}".format(root, args.in_file, args.out_file)
+        cmd = "python {0}/tools/gen_confs.py -i {1} -o {2}".format(root, input_file, output_file)
 
     subprocess.Popen(cmd, shell=True).wait()
+
+    shutil.copyfile(output_file, args.out_file)
+    shutil.rmtree(tmp_dir)
 
 
 def parseArgs() -> argparse.Namespace:
